636151fadf226a2c36b0a14eadabcf52fcd52f01
Beatriz Lopez <beatlm@gmail.com>
2018-12-23 14:56:04 +0100

    Comentado eureka para pruebas con doker y vagrant

--- src/main/java/com/wynats/arq/configurationapi/ConfigurationApiApplication.java
+++ src/main/java/com/wynats/arq/configurationapi/ConfigurationApiApplication.java
@@ -2,10 +2,9 @@ package com.wynats.arq.configurationapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class ConfigurationApiApplication {
 
 	public static void main(String[] args) {
